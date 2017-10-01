ebbbfb9243cf7f69ad6bbcc0f27e0a09bc5f1dc9
Hans Evangelista Perez <hanshavin@LIMD027.local>
2017-10-01 01:12:52 -0500

    add changes for codacy

--- src/ismobile.js
+++ src/ismobile.js
@@ -1,11 +1,11 @@
-const isMobile = {
+const ismobile = {
   Android: () => navigator.userAgent.match(/Android/i),
   BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
   iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
   Opera: () => navigator.userAgent.match(/Opera Mini/i),
   Windows: () => navigator.userAgent.match(/IEMobile/i),
-  any: () => (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() ||
-              isMobile.Opera() || isMobile.Windows()),
+  any: () => (ismobile.Android() || ismobile.BlackBerry() || ismobile.iOS() ||
+              ismobile.Opera() || ismobile.Windows()),
 };
 
-export default isMobile;
+export default ismobile;
