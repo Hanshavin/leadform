a326a8baf273d0edf6f3e0d829457541d37ff41b
Hans Evangelista Perez <hanshavin@LIMD027.local>
2017-10-01 00:55:01 -0500

    add changes to codacy

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         // exclude: /node_modules/,
         // loader: "babel-loader",
         loaders: ['babel-loader', 'eslint-loader'],
-      }
+      },
     ],
-  }
+  },
 };
